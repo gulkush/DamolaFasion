77da3cdc9a08dc80b7e9ea090e706da6e0e2c47c
gulshanbudhwani <gulkush@gmail.com>
2017-01-15 12:24:12 +0530

    package renamed

---
+++ app/src/main/java/com/pathwaymedia/valisimofashions/people_mvvm/view/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.pathwaymedia.valisimofashions.people_mvvm.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pathwaymedia.valisimofashions.people_mvvm.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
